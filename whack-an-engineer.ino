775ead0c73800bb96a4beafe607fa5aff31eda71
Casper Verswijvelt <casperverswijvelt@gmail.com>
2023-11-12 18:27:24 +0100

    Initial version where everything seems to be working

---
+++ whack-an-engineer.ino
@@ -0,0 +1,270 @@
+#include <Keypad.h>
+#include <Adafruit_NeoPixel.h>
+
+#define NEOPIXEL_PIN 12
+#define NUMPIXELS 3
+
+#define RAINBOW_CYCLE_MS 5000
+#define GAME_LENGTH 30000
+
+#define INITIAL_TIME_BETWEEN_LED 750
+#define INITIAL_TIME_LED_ON 750
+#define LED_FADE_MS 75
+#define HIT_FADE_MS 450
+
+
+enum GameState {
+  GAME_IDLE,
+  GAME_STARTING,
+  GAME_PLAYING,
+  GAME_END
+};
+
+// LED configuration
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPIXEL_PIN, NEO_GRBW + NEO_KHZ800);
+
+// Key configuration
+const byte numRows = 2;  //number of rows on the keypad
+const byte numCols = 3;  //number of columns on the keypad
+char keymap[numRows][numCols] = {
+  { '6', '5', '4' },
+  { '3', '2', '1' }
+};
+byte rowPins[numRows] = { 14, 27 };
+byte colPins[numCols] = { 26, 25, 33 };
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+// States
+unsigned long lastGameStateChange = 0;
+
+unsigned long lastLedOn = 0;
+unsigned long lastLedOff = 0;
+unsigned long lastButtonPressed = 0;
+
+unsigned long lastHit = 0;
+
+int previousLedIdx = -1;
+int currentLedIdx = -1;
+int lastHitLedIdx = -1;
+int score = 0;
+bool lastHitWasSucces = false;
+
+GameState state = GAME_IDLE;
+
+int timeBetweenLED = INITIAL_TIME_BETWEEN_LED;
+int timeLEDOn = INITIAL_TIME_LED_ON;
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin();
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  // Key scanning
+  char pressedKey = myKeypad.getKey();
+  bool keyWasPressed = pressedKey != NO_KEY;
+  if (keyWasPressed) {
+    Serial.print("Key pressed: ");
+    Serial.println(pressedKey);
+    lastButtonPressed = currentMillis;
+  }
+
+  switch (state) {
+
+    case GAME_IDLE:
+      {
+        if (keyWasPressed) {
+          setGameState(GAME_STARTING, currentMillis);
+          break;
+        }
+        pixels.rainbow(
+          getStartHue(currentMillis),
+          1,
+          255,
+          min(
+            (currentMillis - lastGameStateChange) / 1250.0 * 255,
+            255.0),
+          true);
+        break;
+      }
+
+    case GAME_STARTING:
+      {
+        // Blocking fade out and game start effect
+
+        // Fade out
+        {
+          int hue = getStartHue(currentMillis);
+          int steps = 200;
+          int stepMs = 1;
+          for (int i = 0; i < steps; i++) {
+            pixels.rainbow(
+              getStartHue(currentMillis),
+              1,
+              255,
+              255 - ((i * 1.0) / steps * 255),
+              true);
+            pixels.show();
+            delay(stepMs);
+          }
+        }
+
+        // 3x red flash
+        {
+          delay(1000);
+          for (int i = 0; i < 3; i++) {
+            int steps = 40;
+            int stepMs = 5;
+            for (int j = 0; j < steps; j++) {
+              pixels.fill(
+                pixels.Color(255.0 / steps * j, 0, 0, 0),
+                0,
+                NUMPIXELS);
+              pixels.show();
+              delay(stepMs);
+            }
+            for (int j = 0; j < steps; j++) {
+              pixels.fill(
+                pixels.Color(255 - 255.0 / steps * j, 0, 0, 0),
+                0,
+                NUMPIXELS);
+              pixels.show();
+              delay(stepMs);
+            }
+            pixels.clear();
+            pixels.show();
+            delay(800);
+          }
+          delay(1000);
+        }
+
+        // Reset game states
+        timeBetweenLED = INITIAL_TIME_BETWEEN_LED;
+        timeLEDOn = INITIAL_TIME_LED_ON;
+        score = 0;
+        lastHitLedIdx = -1;
+        currentLedIdx = -1;
+        previousLedIdx = -1;
+        // Re-read millis since we did blocking stuff
+        setGameState(GAME_PLAYING, millis());
+        break;
+      }
+
+    case GAME_PLAYING:
+      {
+
+        bool gameFinished = currentMillis > lastGameStateChange + GAME_LENGTH;
+
+        // It's possible that we are still in playing state  to let animations
+        //  finish, but the game is already over
+        if (!gameFinished) {
+          // Key handling
+          if (keyWasPressed) {
+            int pressedKeyIdx = String(pressedKey).toInt() - 1;
+            lastHitWasSucces = pressedKeyIdx == currentLedIdx;
+            if (lastHitWasSucces) {
+              // It's a hit!
+              // Turn of LED and increment score
+              turnOffCurrentLED(currentMillis);
+              score++;
+              Serial.print("Score: ");
+              Serial.println(score);
+            } else {
+              Serial.println("Incorrect!");
+            }
+
+            // Save last hit info
+            lastHit = currentMillis;
+            lastHitLedIdx = pressedKeyIdx;
+          }
+
+          // Update game state
+          if (currentLedIdx == -1) {
+            // No LED on right now, check if we should turn one
+            if (currentMillis > (lastLedOff + timeLEDOn)) {
+              // Make sure that new led idx is never same as previous
+              turnOnRandomLED(currentMillis);
+            }
+          } else {
+            // A LED is on right now, check if it should be turned off
+            if (currentMillis > (lastLedOn + timeBetweenLED)) {
+              turnOffCurrentLED(currentMillis);
+            }
+          }
+        }
+
+        // Update LEDs according to game state
+        pixels.clear();
+        if (currentLedIdx != -1) {
+          float brightness = 255.0;
+          int msSinceStart = currentMillis - lastLedOn;
+          int msUntilEnd = timeLEDOn - msSinceStart;
+
+          if (msSinceStart < LED_FADE_MS) {
+            brightness *= msSinceStart * 1.0 / LED_FADE_MS;
+          } else if (msUntilEnd < LED_FADE_MS) {
+            brightness *= msUntilEnd * 1.0 / LED_FADE_MS;
+          }
+          // White LED only
+          pixels.setPixelColor(currentLedIdx, pixels.Color(0, 0, 0, brightness));
+        }
+
+        bool doHitEffect = lastHitLedIdx != -1 && currentMillis < lastHit + HIT_FADE_MS;
+        // Hit effect
+        if (doHitEffect) {
+          float brightness = 255 - 255.0 * (currentMillis - lastHit) / HIT_FADE_MS;
+          pixels.setPixelColor(
+            lastHitLedIdx,
+            lastHitWasSucces
+              ? pixels.Color(0, brightness, 0)
+              : pixels.Color(brightness, 0, 0));
+        }
+
+        // Game end
+        if (
+          // Game time finished
+          gameFinished &&
+          // No LED's on (TODO: and all animations finished)
+          currentLedIdx == -1 && !doHitEffect) {
+          delay(1000);
+          // TODO: Go to GAME_END state instead
+          // Re read millis since we did blocking delay
+          setGameState(GAME_IDLE, millis());
+        }
+        break;
+      }
+  }
+
+  pixels.show();
+}
+
+void setGameState(GameState newGameState, unsigned long millis) {
+  state = newGameState;
+  lastGameStateChange = millis;
+
+  Serial.print("Set game state to ");
+  Serial.println(state);
+}
+
+void turnOnRandomLED(unsigned long millis) {
+  currentLedIdx = random(0, NUMPIXELS - 1);
+  if (currentLedIdx >= previousLedIdx) currentLedIdx++;
+  lastLedOn = millis;
+  Serial.print("Turned on LED with index ");
+  Serial.println(currentLedIdx);
+}
+
+void turnOffCurrentLED(unsigned long millis) {
+  previousLedIdx = currentLedIdx;
+  currentLedIdx = -1;
+  lastLedOff = millis;
+  Serial.println("Turned off LED");
+}
+
+
+int getStartHue(unsigned long currentMillis) {
+  return (currentMillis % RAINBOW_CYCLE_MS) / (RAINBOW_CYCLE_MS * 1.0) * 65535;
+}
