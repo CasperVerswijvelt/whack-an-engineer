1a1d2b7b67dabe962c0306219d3eb4f5b1fdd5a6
Casper Verswijvelt <casperverswijvelt@gmail.com>
2023-11-14 00:29:32 +0100

    More game-like web ui

--- data/script.js
+++ data/script.js
@@ -1,4 +1,23 @@
 const gateway = `ws://${window.location.hostname}/ws`;
+// const gateway = `ws://192.168.69.152/ws`;
+
+const GAME_IDLE = 1;
+const GAME_STARTING = 2;
+const GAME_PLAYING = 3;
+const GAME_END = 4;
+
+const gameStateClassMap = {
+    [GAME_IDLE]: "game--idle",
+    [GAME_STARTING]: "game--starting",
+    [GAME_PLAYING]: "game--playing",
+    [GAME_END]: "game--end",
+}
+
+const formatSeconds = (seconds) => {
+    const date = new Date(0);
+    date.setSeconds(seconds); // specify value for SECONDS here
+    return date.toISOString().substring(14, 19);
+}
 
 const initWebSocket = () => {
     console.log('Trying to open a WebSocket connection...');
@@ -14,7 +33,17 @@ const initWebSocket = () => {
         const id = split[0];
         const value = split[1];
 
-        document.getElementById(id).textContent = value;
+        console.log(formatSeconds(parseInt(value)))
+
+        switch (id) {
+            case "gameState":
+                document.getElementById("game").className = gameStateClassMap[value]
+                break;
+            case "score":
+                document.getElementById("score").innerHTML = value
+            case "time":
+                document.getElementById("clock").innerHTML = formatSeconds(parseInt(value))
+        }
     };
 }
 
